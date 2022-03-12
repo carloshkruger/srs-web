22de46d2c923b447fe8f4ebd74f109b3cd818ab8
carloshkruger <carloshkruger@gmail.com>
2022-03-12 13:52:43 -0300

    chore: created SignIn page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+
+import {
+  Container,
+  Form,
+  ButtonContainer,
+  ForgotPasswordLink,
+  DontHaveAccount,
+  DontHaveAccountLink
+} from './styles'
+import api, { handleApiError } from '../../services/api'
+
+type FormData = {
+  email: string
+  password: string
+}
+
+const validationSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email('E-mail inválido')
+      .required('E-mail é obrigatório'),
+    password: yup
+      .string()
+      .min(6, 'Senha deve possuir no mínimo 6 caracteres')
+      .required('Senha é obrigatório')
+  })
+  .required()
+
+const SignIn: React.FC = () => {
+  const [loading, setLoading] = useState(false)
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: yupResolver(validationSchema)
+  })
+
+  async function onSubmit(data: FormData) {
+    try {
+      setLoading(true)
+
+      await api.post('/v1/auth', data)
+    } catch (error) {
+      handleApiError(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Login</h1>
+
+        <Input
+          name="email"
+          label="E-mail"
+          type="email"
+          register={register}
+          errorMessage={errors.email?.message}
+        />
+        <Input
+          name="password"
+          label="Senha"
+          type="password"
+          register={register}
+          errorMessage={errors.password?.message}
+        />
+        <ForgotPasswordLink to="/forgot-password">
+          Esqueci minha senha
+        </ForgotPasswordLink>
+
+        <ButtonContainer>
+          <Button loading={loading} type="submit">
+            Entrar
+          </Button>
+        </ButtonContainer>
+
+        <DontHaveAccount>
+          Ainda não possui uma conta?{' '}
+          <DontHaveAccountLink to="/signup">Criar conta</DontHaveAccountLink>
+        </DontHaveAccount>
+      </Form>
+    </Container>
+  )
+}
+
+export default SignIn
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes as ReactRoutes } from 'react-router-dom'
 
+import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <ReactRoutes>
       <Route path="signup" element={<SignUp />} />
+      <Route path="signin" element={<SignIn />} />
     </ReactRoutes>
   </BrowserRouter>
 )
